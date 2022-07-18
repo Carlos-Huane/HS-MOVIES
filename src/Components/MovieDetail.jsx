8ace2b6556fe311c23bb0bca212f79c0c94fc298
Carlos-Huane <carloshuanesarmiento@gmail.com>
2022-07-17 22:16:58 -0500

    MovieDetail(EXAMPLE)

--- src/Components/MovieDetail.jsx
+++ src/Components/MovieDetail.jsx
@@ -1,8 +1,41 @@
-const MovieDetail = () => {
-    return (
-        <div>
-            <h1>Soy Movie Detail</h1>
-        </div>
-    )
+// const MovieDetail = () => {
+//     return (
+//         <div>
+//             <h1>Soy Movie Detail</h1>
+//         </div>
+//     )
+// }
+// export default MovieDetail;
+import React from 'react';
+import { connect } from 'react-redux';
+import { getMovieDetail } from '../../actions/index';
+
+import './Movie.css';
+
+export class Movie extends React.Component {
+
+    componentDidMount(){
+        this.props.getMovieDetail(this.props.match.params.id);
+        console.log(this.props.movieDetail);
+    }
+
+    render() {
+        
+        return (
+            <div className="movie-detail">
+                <h1> {this.props.movieDetail.Title} </h1>
+                <h3> {this.props.movieDetail.Actors} </h3>
+                <p>  {this.props.movieDetail.Plot} </p>
+                <img src={this.props.movieDetail.Poster} alt="poster de la pelicula" />
+            </div>
+        );
+    }
 }
-export default MovieDetail;
+
+function mapStateToProps(state){
+    return {
+        movieDetail: state.movieDetail,
+    }
+}
+
+export default connect(mapStateToProps,{getMovieDetail})(Movie);
