808b1acbadb886213533b55e570b02a2b0a6c2ef
Carlos-Huane <carloshuanesarmiento@gmail.com>
2022-07-24 23:33:41 -0500

    24/07/2022

--- src/Components/Favorites.jsx
+++ src/Components/Favorites.jsx
@@ -1,7 +1,24 @@
+ import { useSelector } from "react-redux";
+import Style from "../Styles/Favorites.module.css"
  const Favorites = () => {
+    const moviesFavourites = useSelector(state => state.moviesFavourites);
      return(
          <div>
-             <h1>Soy favorites</h1>
+             <h1>MY FAVORITES MOVIES</h1>
+
+             {moviesFavourites.map(e => {
+                return (
+                    <div className={Style.contenedorCard}>
+                        <div className="text"> 
+                            <h2 style={{writingMode:"vertical-rl"}}>{e.title}</h2>
+                            <button>X</button>
+                        </div>
+                        <div  className={Style.card} key={e.id}  style={{backgroundImage:`url(${e.poster})`}}> 
+                            
+                        </div>
+                    </div>
+                )
+             })}
          </div>
      )
  }
--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -5,6 +5,7 @@ import Style from '../Styles/Inicio.module.css'
 import { useDispatch } from 'react-redux';
 const Inicio = ( )=> {
     const state = useSelector(state => state.moviesLoaded)
+    const estadoGeneral = useSelector(state=>state);
     const dispatch = useDispatch();
     console.log(state.moviesLoaded);
     return(
@@ -17,14 +18,14 @@ const Inicio = ( )=> {
                     </div>
             <div className={Style.titleFav}>
               <NavLink to={`/movie/${movie.imdbID}`} className={Style.text}> <h3>{movie.Title}</h3> </NavLink> 
-              <span>
+              <button onClick={() => dispatch(addMovieFavorite({title:movie.Title, id:movie.imdbID, poster:movie.Poster}))  }>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                   <circle cx="12" cy="12" r="9" />
                   <line x1="9" y1="12" x2="15" y2="12" />
                   <line x1="12" y1="9" x2="12" y2="15" />
                 </svg>  
-              </span>
+              </button>
             </div>  
                     
                     {/* <h2 className={Style.text}>{movie.Title}</h2> */}
