dd96a122c250c204b3b26e2642c01a6d01250dbb
Carlos-Huane <carloshuanesarmiento@gmail.com>
2022-07-28 19:10:32 -0500

    Deploying

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,17 +1,17 @@
-import { GET_MOVIES } from "./ActionTypes.js";
+import { GET_MOVIES, GET_MOVIE_DETAIL, REMOVE_MOVIE_FAVORITE, ADD_MOVIE_FAVORITE } from "./ActionTypes.js";
 
-const apiKey = "a77f0041";
+const apiKey = process.env.apiKey;
 
 export function addMovieFavorite(payload) {
-  return { type: "ADD_MOVIE_FAVORITE", payload };
+  return { type: ADD_MOVIE_FAVORITE, payload };
 } 
 
 export function getMovies(titulo) {
     return function(dispatch) {
-      return fetch("http://www.omdbapi.com/?apikey=a77f0041&s=" + titulo)
+      return fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=` + titulo)
         .then(response => response.json())
         .then(json => {
-          dispatch({ type: "GET_MOVIES", payload: json });
+          dispatch({ type: GET_MOVIES, payload: json });
         });
     };
   }
@@ -19,13 +19,13 @@ export function getMovies(titulo) {
   export function getMovieDetail(id){
     
     return function(dispatch){
-      return fetch("http://www.omdbapi.com/?apikey=a77f0041&i="+id)
+      return fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=`+id)
         .then(response => response.json())
         .then(json => {
-          dispatch({type: "GET_MOVIE_DETAIL" , payload:json})
+          dispatch({type: GET_MOVIE_DETAIL, payload:json})
         });
     };
   }
   export function removeMovieFavorite(id){
-    return { type: "REMOVE_MOVIE_FAVORITE", payload:id}
+    return { type: REMOVE_MOVIE_FAVORITE, payload:id}
   }
